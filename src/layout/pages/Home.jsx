b037a153de6cf2e79b4115bfe5ecd8fbbcdc4297
Maëlle <maelle.r.baudotgmail.com>
2026-05-06 15:55:30 +0200

    images db à corriger

--- src/layout/pages/Home.jsx
+++ src/layout/pages/Home.jsx
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-
 export const Home =()=>{
-   
-=======
-import { useNavigate } from "react-router"
 
-export const Home =()=>{
-    const navigate = UseNavigate(); 
->>>>>>> 9019c73df3a1ad47839f55d3324d7a624fc455d1
+
     return(
     <>
     <section className="py-12 px-44 flex flex-col gap-4 items-start text-main-800">
