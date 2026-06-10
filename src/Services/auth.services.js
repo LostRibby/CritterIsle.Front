61814239d5fee1c0626b411ad08865123b0bcced
Maëlle <maelle.r.baudotgmail.com>
2026-06-10 11:51:42 +0200

    test urls connexion

--- src/Services/auth.services.js
+++ src/Services/auth.services.js
@@ -4,11 +4,11 @@ import axios from "axios";
 const authService = {
 
     register: async(formData) => {
-        const response = await axios.post('http://localhost:3000/api/auth/register', formData); 
+        const response = await axios.post('https://critterisle-back-2.onrender.com/auth/register', formData); 
         return response.data;
     }, 
     login: async({email, password}) => {
-        const response = await axios.post('http://localhost:3000/api/auth/login', {email, password}); 
+        const response = await axios.post('https://critterisle-back-2.onrender.com/api/auth/login', {email, password}); 
         return response.data
     }
     
