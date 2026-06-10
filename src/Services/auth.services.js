65ac66f0e4758aa11f644b8569161686fd069e59
Maëlle <maelle.r.baudotgmail.com>
2026-06-10 14:01:47 +0200

    correction register page

--- src/Services/auth.services.js
+++ src/Services/auth.services.js
@@ -3,10 +3,26 @@ import axios from "axios";
 
 const authService = {
 
-    register: async(formData) => {
-        const response = await axios.post('https://critterisle-back-2.onrender.com/api/auth/register', formData); 
+    register: async (formData) => {
+    try {
+        const response = await axios.post(
+            'https://critterisle-back-2.onrender.com/api/auth/register',
+            formData
+        );
         return response.data;
-    }, 
+
+    } catch (error) {
+        if (error.response) {
+            if (error.response.status === 409) {
+                throw new Error("Cet utilisateur existe déjà (409)");
+            }
+            
+            throw new Error(error.response.data?.message || "Erreur serveur");
+        }
+
+        throw new Error("Erreur de connexion au serveur");
+    }
+},
     login: async({email, password}) => {
         const response = await axios.post('https://critterisle-back-2.onrender.com/api/auth/login', {email, password}); 
         return response.data
