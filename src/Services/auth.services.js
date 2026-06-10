098d0bad6bb4e2bf5f85188f0fefb132a3c34b49
Maëlle <maelle.r.baudotgmail.com>
2026-06-10 13:05:42 +0200

    urls fixes bis bis

--- src/Services/auth.services.js
+++ src/Services/auth.services.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const authService = {
 
     register: async(formData) => {
-        const response = await axios.post('https://critterisle-back-2.onrender.com/auth/register', formData); 
+        const response = await axios.post('https://critterisle-back-2.onrender.com/api/auth/register', formData); 
         return response.data;
     }, 
     login: async({email, password}) => {
