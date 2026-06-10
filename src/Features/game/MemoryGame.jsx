3e1d0246dea044b9e8c59ce6d0507ab4ed5bbeeb
Maëlle <maelle.r.baudotgmail.com>
2026-06-10 12:01:20 +0200

    fix url bis bis

--- src/Features/game/MemoryGame.jsx
+++ src/Features/game/MemoryGame.jsx
@@ -20,7 +20,7 @@ export default function MemoryGame() {
     const fetchBugs = async () => {
         try {
             const response = await axios.get(
-                "http://localhost:3000/api/Bugs"
+                "https://critterisle-back-2.onrender.com/api/Bugs"
             );
 
             const bugCards = response.data.data.map(bug => ({
